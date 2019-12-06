6235119fb9c50b28382577f06d74910f29a1587c
neale <nealeratzlaff@gmail.com>
2019-12-05 17:40:02 -0800

    made changes to sampler and to hyper_heads to add random aux noise to each particle, while having the same one hot draw, should lead to better uncertainty. If these breaks, the commit before this one has working code

--- deep_rl/network/hyper_heads.py
+++ deep_rl/network/hyper_heads.py
@@ -50,13 +50,13 @@ class DuelingHyperNet(nn.Module, BaseNet):
         self.z_dim = self.config['z_dim']
         self.n_gen = self.config['n_gen'] + self.features.config['n_gen'] + 1
         self.particles = Config.particles
-        self.noise_sampler = NoiseSampler(dist, (self.z_dim,))
+        self.noise_sampler = NoiseSampler(dist, self.z_dim, particles)
         # self.sample_model_seed()
         self.to(Config.DEVICE)
     
     def sample_model_seed(self):
         sample_z = self.noise_sampler.sample().to(Config.DEVICE)
-        sample_z = sample_z.unsqueeze(0).unsqueeze(0).repeat(self.features.config['n_gen'], self.particles, 1)
+        sample_z = sample_z.unsqueeze(0).repeat(self.features.config['n_gen'], 1, 1)
         self.model_seed = {
             'features_z': sample_z,
             'value_z': sample_z[0],
