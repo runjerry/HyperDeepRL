4633a0813b8f2426c1a7a835329d1a8ed9b27d7f
Neale Ratzlaff <nealeratzlaff@gmail.com>
2019-12-09 14:38:40 -0800

    added better HP sampler

--- hyperexamples.py
+++ hyperexamples.py
@@ -5,14 +5,69 @@
 #######################################################################
 
 from deep_rl import *
+import itertools
 import envs
 
-def load_args():
-    parser = argparse.ArgumentParser(description='main args')
-    parser.add_argument('--tb_tag', default='', type=str)
-    args = parser.parse_args()
-    return args
+def product_dict(kwargs):
+    keys = kwargs.keys()
+    vals = kwargs.values()
+    for instance in itertools.product(*vals):
+        yield dict(zip(keys, instance))
+   
+def sweep(game, tag, model_fn, trials=50, manual=True):
+    hyperparams = {
+        'alpha_i': [0.01, 0.1, 1, 10],
+        'alpha_f': [0.01, 0.1, 1, 10],
+        'anneal': [500e3],
+        'lr': [1e-2, 1e-3],
+        'freq' : [10, 25],
+        'grad_clip': [None, 1],
+        'hidden': [256, 128],
+        'replay_size': [int(1e3), int(1e4)],
+        'replay_bs': [32, 64, 128],
+        'dist': ['categorical', 'multinomial', 'normal', 'uniform']
+    }
+    # manually define
+    if manual:
+        print ('=========================================================')
+        print ('Running Manually Defined Single Trial, [1/1]')
+        setting = {
+            'game': game,
+            'tb_tag': tag,
+            'alpha_i': 0.1,
+            'alpha_f': 0.1,
+            'anneal': 500e3,
+            'lr': 0.01,
+            'freq': 10,
+            'grad_clip': None,
+            'hidden': 256,
+            'replay_size': int(1e3),
+            'replay_bs': 32,
+            'dist': 'categorical'
+        }
+        print ('Running Config: ')
+        for (k, v) in setting.items():
+            if setting['alpha_f'] > setting['alpha_i']:
+                continue
+            print ('{} : {}'.format(k, v))
+        model_fn(**setting)
+        return
 
+    search_space = list(product_dict(hyperparams))
+    ordering = list(range(len(search_space)))
+    np.random.shuffle(ordering)
+    for i, idx in enumerate(ordering):
+        setting = search_space[idx]
+        setting['game'] = game
+        setting['tb_tag'] = tag
+        print ('=========================================================')
+        print ('Search Space Contains {} Trials, Running [{}/{}] ---- ({}%)'.format(
+            len(search_space), i+1, trials, int(float(i+1)/trials*100.)))
+        print ('Running Config: ')
+        for (k, v) in setting.items():
+            print ('{} : {}'.format(k, v))
+        dqn_feature(**setting)
+    
 # DQN Toy Example
 def dqn_toy_feature(**kwargs):
     generate_tag(kwargs)
@@ -30,7 +85,7 @@ def dqn_toy_feature(**kwargs):
 
     config.random_action_prob = LinearSchedule(0.01, 0.001, 1e4)
     config.discount = 0.8
-    config.target_network_update_freq = 10
+    config.target_network_update_freq = config.freq
     config.exploration_steps = 0
     config.double_q = True
     config.sgd_update_frequency = 4
@@ -53,45 +108,5 @@ if __name__ == '__main__':
     # select_device(-1)
     select_device(0)
     args = load_args()
-
-    switch = 4
-
-    if switch == 1:
-        a = [1e2]   
-        learning_rate = [1e-2, 1e-3, 5e-4, 2e-4, 1e-4]
-        replay_memory_size = [1e3, 1e5]
-        replay_batch_size = [32, 128]
-
-    if switch == 2:
-        a = [1e1]   
-        learning_rate = [1e-2, 1e-3, 5e-4, 2e-4, 1e-4]
-        replay_memory_size = [1e3, 1e5]
-        replay_batch_size = [32, 128]
-
-    if switch == 3:
-        a = [1e-1]   
-        learning_rate = [1e-2, 1e-3, 5e-4, 2e-4, 1e-4]
-        replay_memory_size = [1e3, 1e5]
-        replay_batch_size = [32, 128]
-
-    if switch == 4:
-        a = [1e-3, 1]   
-        learning_rate = [1e-2, 1e-3, 5e-4, 2e-4, 1e-4]
-        replay_memory_size = [1e3, 1e5]
-        replay_batch_size = [32, 128]
+    sweep('NChain-v3', 'ChainSweep', dqn_toy_feature, trials=50, manual=True)
     
-
-    game = 'NChain-v3'
-    for alpha in a:
-        for lr in learning_rate:
-            for replay_memory in replay_memory_size:
-                for replay_bs in replay_batch_size:
-                    for i in np.linspace(20, 100, 81)[::2]:
-                        i = int(i)
-                        dqn_toy_feature(game=game,
-                                        tb_tag='alpha{}-lr{}-rm{}-bs{}N{}'.format(alpha,lr,replay_memory,replay_bs,i),
-                                        chain_len=i,
-                                        alpha=alpha,
-                                        lr=lr,
-                                        replay_memory=replay_memory,
-                                        replay_bs=replay_bs)
