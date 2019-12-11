e3ac1d2c24748f61428cea3feb7d5395e4b7b34a
Neale Ratzlaff <nealeratzlaff@gmail.com>
2019-12-11 15:15:31 -0800

    minor change to samplers, mostly formattting

--- deep_rl/component/samplers.py
+++ deep_rl/component/samplers.py
@@ -17,18 +17,22 @@ class NoiseSampler(object):
             high = torch.ones(self.z_dim)
             low = -1 * high
             self.base_dist = torch.distributions.Uniform(low, high)
+
         elif self.dist_type == 'normal':
             loc = torch.zeros(self.z_dim)
             scale = torch.ones(self.z_dim)
             self.base_dist = torch.distributions.Normal(loc, scale)
+        
         elif self.dist_type == 'bernoulli':
             k_classes = torch.ones(self.z_dim)
             probs = k_classes * .5
             self.base_dist = torch.distributions.Bernoulli(probs=probs)
+        
         elif self.dist_type == 'categorical':
             k_classes = self.z_dim
             probs = torch.ones(k_classes)/float(k_classes)
             self.base_dist = torch.distributions.OneHotCategorical(probs=probs)
+        
         elif self.dist_type == 'softmax':
             k_classes = self.z_dim
             probs = torch.ones(k_classes)/float(k_classes)
@@ -38,17 +42,19 @@ class NoiseSampler(object):
             #high = torch.ones(self.particles, self.z_dim) * .005
             #low = torch.zeros(self.particles, self.z_dim)
             self.aux_dist = torch.distributions.Uniform(low, high)
+        
         elif self.dist_type == 'multinomial':
             total_count = self.z_dim
             probs = torch.ones(self.z_dim)
             self.base_dist = torch.distributions.Multinomial(total_count, probs)
+        
         elif self.dist_type == 'multivariate_normal':
             loc = torch.zeros(self.z_dim)
             rng_mat = torch.rand(self.z_dim, self.z_dim)
             psd_mat = torch.mm(rng_mat, rng_mat.t())
             cov = psd_mat
             self.base_dist = torch.distributions.MultivariateNormal(loc, cov)
-
+   
     def sample(self):
         sample = self.base_dist.sample()
         if self.aux_dist is not None:
